8f5681fa67b872e8768d06c324621cf6557c79b7
takyokoy <takyokoy@yahoo-corp.jp>
2017-01-07 23:58:31 +0900

    Initial Commit

---
+++ ScrapingKit/ScrapingKit.h
@@ -0,0 +1,19 @@
+//
+//  ScrapingKit.h
+//  ScrapingKit
+//
+//  Created by takyokoy on 2017/01/07.
+//  Copyright © 2017年 chocoyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScrapingKit.
+FOUNDATION_EXPORT double ScrapingKitVersionNumber;
+
+//! Project version string for ScrapingKit.
+FOUNDATION_EXPORT const unsigned char ScrapingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrapingKit/PublicHeader.h>
+
+
